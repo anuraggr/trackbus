47af0459bc0b713073837b092159e15382353477
Anurag Rai <129479513+anuraggr@users.noreply.github.com>
2024-09-22 05:05:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -223,8 +223,8 @@ function App() {
               >
                 Refresh
               </button>
-
-              /*<button
+              <!--
+              <button
                 style={{
                   position: 'absolute',
                   bottom: '10px',
@@ -240,7 +240,8 @@ function App() {
                 onClick={handleUserLoc}
               >
                 Me
-              </button>*/
+              </button>
+                -->
             </div>
           )}
         </div>
